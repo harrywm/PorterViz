ddb9a8df3310c4f4d432767cde210be4f76ffee5
Harry morgan <harrywm12@gmail.com>
2020-12-22 12:53:54 +0000

    Full Container by Network - functionally

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"github.com/kr/pretty"
 )
+
+var cli = refreshClient()
+var containers = containerList(cli)
+var networks = networkList(cli)
 
 func refreshClient() *client.Client {
 
@@ -14,31 +19,58 @@ func refreshClient() *client.Client {
 	if err != nil {
 		panic(err)
 	}
+
 	return cli
 }
 
-func networkList(cli *client.Client) {
+func containerList(cli *client.Client) []types.Container {
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	return containers
+}
+
+func networkList(cli *client.Client) []types.NetworkResource {
 
 	networks, err := cli.NetworkList(context.Background(), types.NetworkListOptions{})
 	if err != nil {
 		panic(err)
 	}
 
-	// THIS WORKS FOR LISTING FULL INSPECT OF ALL NETWORKS
-	for _, network := range networks {
-		//fmt.Println(network.ID)
-		networkInspect, err := cli.NetworkInspect(context.Background(), network.ID, types.NetworkInspectOptions{})
-		if err != nil {
-			panic(err)
-		}
-		if len(networkInspect.Containers) != 0 {
-			fmt.Println(networkInspect.Containers)
+	return networks
+}
+
+func getByNetwork(network types.NetworkResource) []types.Container {
+	var netContainers []types.Container
+
+	networkInspect, err := cli.NetworkInspect(context.Background(), network.ID, types.NetworkInspectOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	// If Network has Containers
+	if len(networkInspect.Containers) != 0 {
+		// For each container in the network
+		for _, container := range networkInspect.Containers {
+			// Retrieve full Inspect of Container by Name from Global Container List
+			for i := range containers {
+				if containers[i].Names[0][1:] == container.Name {
+					netContainers = append(netContainers, containers[i])
+				}
+			}
 		}
 	}
+
+	fmt.Printf("%# v", pretty.Formatter(netContainers))
+	return netContainers
+
 }
 
 func main() {
 
-	networkList(refreshClient())
+	getByNetwork(networks[0])
 
 }
