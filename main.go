bebe40f491f7b1e218c3acc1a840ff9fc4fbc3de
Harry morgan <harrywm12@gmail.com>
2021-01-11 11:42:44 +0000

    Relocated types to main.go, added population, bit of cleanup

--- main.go
+++ main.go
@@ -7,9 +7,41 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/kr/pretty"
-
-	porternet "porter/donut/porternets"
 )
+
+// PorterContainer is an abstracted version of the Docker data type for a Container containing only the fields we are concerned with
+type PorterContainer struct {
+	Name    []string
+	ID      string
+	Image   string
+	ImageID string
+	Ports   []types.Port
+	State   string
+}
+
+// PorterNetwork is an abstracted version of the Docker Network data type containing only the fields we are concerned with
+type PorterNetwork struct {
+	Name       string
+	ID         string
+	Driver     string
+	Containers []PorterContainer
+}
+
+func (n *PorterNetwork) getInfo() string {
+	return n.Name
+}
+
+func (n *PorterNetwork) getID() string {
+	return n.ID
+}
+
+func (n *PorterNetwork) getDriver() string {
+	return n.Driver
+}
+
+func (n *PorterNetwork) getContainers() []PorterContainer {
+	return n.Containers
+}
 
 var cli = refreshClient()
 var containers = containerList(cli)
@@ -65,41 +97,64 @@ func getByNetwork(network types.NetworkResource) []types.Container {
 			}
 		}
 	}
-	fmt.Printf("%# v", pretty.Formatter(network))
-	fmt.Printf("%# v", pretty.Formatter(netContainers))
 	return netContainers
 }
 
-func allNetworks() []porternet.PorterNetwork {
+func allNetworks() []PorterNetwork {
 
-	var returnNets []porternet.PorterNetwork
+	var returnNets []PorterNetwork = []PorterNetwork{}
+	var net PorterNetwork = PorterNetwork{}
+	var cont []PorterContainer = []PorterContainer{}
 
 	for _, network := range networks {
+
 		currentContainer := getByNetwork(network)
 
-		net := porternet.PorterNetwork{
-			network.Name
-			network.ID
-			network.Driver
-			{
-				currentContainer.Name
-				currentContainer.ID
-				currentContainer.Ports
-				currentContainer.State
-			}
+		for _, container := range currentContainer {
+
+			cont = append(cont,
+				PorterContainer{
+					Name:    container.Names,
+					ID:      container.ID,
+					Image:   container.Image,
+					ImageID: container.ImageID,
+					Ports:   container.Ports,
+					State:   container.State,
+				})
+		}
+
+		net = PorterNetwork{
+			Name:       network.Name,
+			ID:         network.ID,
+			Driver:     network.Driver,
+			Containers: cont,
 		}
 
 		returnNets = append(returnNets, net)
-
 	}
 
 	return returnNets
-
 }
 
 func main() {
 
-	fmt.Printf("%# v", allNetworks())
+	networks := allNetworks()
+
+	conts := networks[0].getContainers()
+
+	for _, cont := range conts {
+
+		contInsp, err := cli.ContainerInspect(context.Background(), cont.ID)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("%# v", pretty.Formatter(contInsp))
+
+	}
+
+	fmt.Printf("%# v", pretty.Formatter(networks))
 
 	//js := mewn.String("./frontend/build/static/js/main.js")
 	//css := mewn.String("./frontend/build/static/css/main.css")
